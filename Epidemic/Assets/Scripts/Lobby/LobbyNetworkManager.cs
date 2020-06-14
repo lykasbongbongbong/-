76e4c4181fa958bedf52b8f2d6364daae2e5b80c
HahaRiku <s9323839@gmail.com>
2020-06-15 00:14:37 +0800

    網路部分先commit

--- Epidemic/Assets/Scripts/Lobby/LobbyNetworkManager.cs
+++ Epidemic/Assets/Scripts/Lobby/LobbyNetworkManager.cs
@@ -6,11 +6,15 @@ using System;
 using UnityEngine.SceneManagement;
 
 public class LobbyNetworkManager : NetworkManager {
+    [SerializeField] private int minPlayers = 2;
+
     [Scene] [SerializeField] private string lobby = string.Empty;
     [SerializeField] private NetworkRoomPlayerLobby roomPlayerPrefab = null;
 
     public static event Action OnClientConnected;
     public static event Action OnClientDisconnected;
+
+    public List<NetworkRoomPlayerLobby> RoomPlayers { get; } = new List<NetworkRoomPlayerLobby>();
 
     public override void OnClientConnect(NetworkConnection conn) {
         base.OnClientConnect(conn);
@@ -36,8 +40,40 @@ public class LobbyNetworkManager : NetworkManager {
 
     public override void OnServerAddPlayer(NetworkConnection conn) {
         if(SceneManager.GetActiveScene().name == lobby) {
+            bool isLeader = RoomPlayers.Count == 0;
             NetworkRoomPlayerLobby roomPlayerInstance = Instantiate(roomPlayerPrefab);
+            roomPlayerInstance.IsLeader = isLeader;
             NetworkServer.AddPlayerForConnection(conn, roomPlayerInstance.gameObject);
         }
+    }
+
+    public override void OnServerDisconnect(NetworkConnection conn) {
+        if(conn.identity != null) {
+            var player = conn.identity.GetComponent<NetworkRoomPlayerLobby>();
+            RoomPlayers.Remove(player);
+            UpdatePlayersReadyState();
+        }
+        base.OnServerDisconnect(conn);
+    }
+
+    public override void OnStopServer() {
+        RoomPlayers.Clear();
+    }
+
+    public void UpdatePlayersReadyState() {
+        foreach(var player in RoomPlayers) {
+            player.HandleReadyToStart(IsReadyToStart());
+        }
+    }
+
+    private bool IsReadyToStart() {
+        if (numPlayers < minPlayers) return false;
+
+        foreach(var player in RoomPlayers) {
+            if((!player.IsReady)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
--- Epidemic/Assets/Scripts/Lobby/NetworkRoomPlayerLobby.cs
+++ Epidemic/Assets/Scripts/Lobby/NetworkRoomPlayerLobby.cs
@@ -4,5 +4,75 @@ using UnityEngine;
 using Mirror;
 
 public class NetworkRoomPlayerLobby : NetworkBehaviour {
-    
+    [SyncVar(hook = nameof(HandleReadyStatusChanged))]
+    public bool IsReady = false;
+
+    private bool isLeader;
+
+    public bool IsLeader {
+        set {
+            isLeader = value;
+            //set start game button
+        }
+    }
+
+    private LobbyNetworkManager room;
+    private LobbyNetworkManager Room {
+        get {
+            if (room != null) return room;
+            return room = NetworkManager.singleton as LobbyNetworkManager;
+        }
+    }
+
+    public override void OnStartAuthority() {
+        
+    }
+
+    public override void OnStartClient() {
+        Room.RoomPlayers.Add(this);
+        //UpdateDisplay() ;
+    }
+
+    public override void OnStopClient() {
+        Room.RoomPlayers.Remove(this);
+        //UpdateDisplay() ;
+    }
+
+    public void HandleReadyStatusChanged(bool oldValue, bool newValue) {
+        //UpdateDisplay();
+    }
+
+    /*private void UpdateDisplay() {
+        if (!hasAuthority) {
+            foreach (var player in Room.RoomPlayers) {
+                if (player.hasAuthority) {
+                    player.UpdateDisplay();
+                    break;
+                }
+            }
+
+            return;
+        }
+
+        for (int i = 0; i < playerNameTexts.Length; i++) {
+            playerNameTexts[i].text = "Waiting For Player...";
+            playerReadyTexts[i].text = string.Empty;
+        }
+
+        for (int i = 0; i < Room.RoomPlayers.Count; i++) {
+            playerNameTexts[i].text = Room.RoomPlayers[i].DisplayName;
+            playerReadyTexts[i].text = Room.RoomPlayers[i].IsReady ?
+                "<color=green>Ready</color>" :
+                "<color=red>Not Ready</color>";
+        }
+    }*/
+
+    public void HandleReadyToStart(bool b) {
+
+    }
+
+    [Command]
+    private void CmdReadyUp() {
+
+    }
 }
