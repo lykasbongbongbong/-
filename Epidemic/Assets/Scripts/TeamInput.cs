5cfce9e0fd723f23298a4fbca82674ded7a36d96
HahaRiku <s9323839@gmail.com>
2020-06-15 19:38:14 +0800

    [更新]TeamLocal加上UMA

--- Epidemic/Assets/Scripts/TeamInput.cs
+++ Epidemic/Assets/Scripts/TeamInput.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UMA.CharacterSystem;
 
 public class TeamInput : MonoBehaviour {
     public int playerNum;
@@ -12,18 +13,43 @@ public class TeamInput : MonoBehaviour {
     public Button readyButton;
     public Text readyText;
     public Text anotherReadyText;
+    public Text characterChoiceText;
+    public DynamicCharacterAvatar avatar;
 
     private string inputName;
+    private int characterIndex = 1;
 
     // Start is called before the first frame update
     void Start() {
         readyText.enabled = false;
         readyButton.interactable = false;
+        characterChoiceText.text = characterIndex.ToString();
     }
 
     // Update is called once per frame
     void Update() {
-        
+        Debug.Log(avatar.activeRace.name);
+    }
+
+    public void CharacterNext() {
+        characterIndex = (characterIndex == 2) ? 1 : characterIndex+1;
+        characterChoiceText.text = characterIndex.ToString();
+        UpdateCharacter();
+    }
+
+    public void CharacterPrevious() {
+        characterIndex = (characterIndex == 1) ? 2 : characterIndex-1;
+        characterChoiceText.text = characterIndex.ToString();
+        UpdateCharacter();
+    }
+
+    private void UpdateCharacter() {
+        if(characterIndex==1) {
+            avatar.ChangeRace("HumanMale");
+        }
+        else {
+            avatar.ChangeRace("HumanFemale");
+        }
     }
 
     public void UpdateInputField(string str) {
